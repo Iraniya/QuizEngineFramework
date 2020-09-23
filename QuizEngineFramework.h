255024981170361e68c3393958f8d104d498e2c1
iraniya <naynesh@whadatime.com>
2020-09-23 16:16:35 +0530

    initial commit

---
+++ QuizEngineFramework.h
@@ -0,0 +1,19 @@
+//
+//  QuizEngineFramework.h
+//  QuizEngineFramework
+//
+//  Created by iraniya on 23/09/20.
+//  Copyright © 2020 iraniya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuizEngineFramework.
+FOUNDATION_EXPORT double QuizEngineFrameworkVersionNumber;
+
+//! Project version string for QuizEngineFramework.
+FOUNDATION_EXPORT const unsigned char QuizEngineFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuizEngineFramework/PublicHeader.h>
+
+
